1e76f2858d3c90f9805040592d79a5f42487c211
unknown <rajonjules13@gmail.com>
2021-10-11 20:48:05 +0800

    Ayaw pa rin gumana!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent} from '../app/login/login.component';
 const routes: Routes = [
-  {path:"login", component: LoginComponent},
-  {path:"", component: LoginComponent},
+  {path:"", redirectTo: "/login", pathMatch: "full"},
+  {path:"login", component: LoginComponent}
 ];
 
 @NgModule({
